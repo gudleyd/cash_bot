258e79c02ce1e10a87807f70a6c847ffe42ac50c
Ivan Lebedev <van4es0909@gmail.com>
2017-08-17 16:16:27 +0300

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -5,17 +5,56 @@
 #m_ready - Сообщение о готовности (после /start)
 
 lan = {}
-lan['English'] = {'bar_error': 'The purchase need to be entered like <Bargain, Price>',
+lan['English'] = {'bar_error': 'The purchase need to be entered like <Bargain Price>',
                   'b_his': u'\U0001F4D3',
                   'b_set': u'\U0001F527',
                   'b_del_his': u'\U0001F5D1',
                   'm_ready': 'Everything is ready',
                   'm_del_his': 'History deleted',
-                  'm_mon': 'Budget: '}
+                  'm_mon': 'Budget: ',
+                  'm_ExcelChoose': 'History for:\n\n'
+                                   '1 Month\n'
+                                   '{}\n'
+                                   ''
+                                   '3 Months\n'
+                                   '{}\n'
+                                   ''
+                                   'Year\n'
+                                   '{}\n'
+                                   ''
+                                   'All Time\n'
+                                   '{}',
+                  'm_lastBars': 'Last 16 actions',
+                  'm_bigNumber': 'Price of bargain is very big',
+                  'm_heap': 'Heap',
+                  'm_total': 'Total',
+                  'm_price': 'Price',
+                  'm_name': 'Name',
+                  'm_excelTimer': 'You have recently created a table by {}, wait {} hours {} minutes {} seconds'
+                  }
 lan['Русский'] = {'bar_error': 'Покупку надо вводить по примеру <Покупка Цена>',
                   'b_his': u'\U0001F4D3',
                   'b_set': u'\U0001F527',
                   'b_del_his': u'\U0001F5D1',
                   'm_ready': 'Все готово!',
                   'm_del_his': 'История стерта',
-                  'm_mon': 'Деньги: '}
+                  'm_mon': 'Бюджет: ',
+                  'm_ExcelChoose': 'История за:\n\n'
+                                   '1 Месяц\n'
+                                   '{}\n'
+                                   ''
+                                   '3 Месяца\n'
+                                   '{}\n'
+                                   ''
+                                   'Год\n'
+                                   '{}\n'
+                                   ''
+                                   'Все время\n'
+                                   '{}',
+                  'm_lastBars': 'Последние 16 действий',
+                  'm_bigNumber': 'Сумма покупки слишком велика',
+                  'm_heap': 'Куча',
+                  'm_total': 'Итого',
+                  'm_price': 'Цена',
+                  'm_name': 'Название',
+                  'm_excelTimer': 'Вы уже недавно создавали отчет {}, подождите {} часов {} минут {} секунд'}
